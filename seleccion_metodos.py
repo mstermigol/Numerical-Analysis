ff6f271149593d0ae428c683a194f02737e1e978
mstermigol <migueljaramillo879@gmail.com>
2023-05-30 22:12:04 -0500

    LU simple

--- seleccion_metodos.py
+++ seleccion_metodos.py
@@ -13,6 +13,7 @@ from Matrices.gseidel import gseidel
 from Matrices.LUpar import LUparcial
 from Matrices.Crout import crout
 from Matrices.Doolitle import doolitle_solucion
+from Matrices.LUsimpl import LUsimpl
 import numpy as np
 from math import *
 
@@ -143,8 +144,6 @@ def definirMetodo(root, metodo):
         calcular = tk.Button(root, text="Calcular", command=lambda: busquedas(
             funcionEntry.get(), float(x0Entry.get()), float(hEntry.get()), int(NmaxEntry.get()), root, operaciones, atras))
         calcular.grid()
-
-        print("Busqueda incremental")
     elif metodo == "Newton":
         label = tk.Label(root, text="Newton", font=("Arial", 20))
         label.grid()
@@ -177,8 +176,6 @@ def definirMetodo(root, metodo):
         calcular = tk.Button(root, text="Calcular", command=lambda: newton_lambda(
             fEntry.get(), derivadaEntry.get(), float(x0Entry.get()), float(tolEntry.get()), int(NmaxEntry.get()), root, operaciones, atras))
         calcular.grid()
-
-        print("Newton")
     elif metodo == "Punto fijo":
         print("Punto fijo")
     elif metodo == "Raices multiples":
@@ -371,7 +368,6 @@ def definirMetodo(root, metodo):
         x_result = tk.Label(root, text="Valores de x:")
         x_result.grid()
 
-        print("Gauss con pivoteo parcial")
     elif metodo == "Gauss con pivoteo total":
         print("Gauss con pivoteo total")
     elif metodo == "Gauss-Seidel":
@@ -438,7 +434,6 @@ def definirMetodo(root, metodo):
 
         error_result = tk.Label(root, text="Error:")
         error_result.grid()
-        print("Gauss-Seidel")
     elif metodo == "Jacobi":
         def calcularJacobi(A_str, b_str, X0_str, tol_str, Nmax_str):
             A_rows = A_str.split(';')
@@ -540,9 +535,42 @@ def definirMetodo(root, metodo):
 
         x_result = tk.Label(root, text="Valores de x:")
         x_result.grid()
-        print("LU parcial")
     elif metodo == "LU simple":
-        print("LU simple")
+        def calcularLuSimple(A_str, b_str):
+            A_rows = A_str.split(';')
+            A_values = [list(map(float, row.split(','))) for row in A_rows]
+            A = np.array(A_values)
+            b = np.array(list(map(float, b_str.split(','))))
+
+            x = LUsimpl(A, b)
+
+            x_result.config(text="Valores de x: " + str(x))
+
+        label = tk.Label(root, text="LU Simple", font=("Arial", 20))
+        label.grid()
+
+        A_label = tk.Label(
+            root, text="Ingrese la matriz A (separada por comas, filas por punto y coma):")
+        A_label.grid()
+        A_entry = tk.Entry(root)
+        A_entry.grid()
+
+        b_label = tk.Label(
+            root, text="Ingrese el vector b (separado por comas):")
+        b_label.grid()
+        b_entry = tk.Entry(root)
+        b_entry.grid()
+
+        calcular = tk.Button(root, text="Calcular", command=lambda: calcularLuSimple(
+            A_entry.get(), b_entry.get()))
+        calcular.grid()
+
+        result_label = tk.Label(root, text="Resultados:")
+        result_label.grid()
+
+        x_result = tk.Label(root, text="Valores de x:")
+        x_result.grid()
+        
     elif metodo == "LaGrange":
         label = tk.Label(root, text="LaGrange", font=("Arial", 20))
         label.grid()
