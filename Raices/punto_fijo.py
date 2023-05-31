3b5f67278e039c4b564f97fd7323f815fc6e3251
Miguel <miguelsosa2003@gmail.com>
2023-05-31 13:29:50 -0500

    correccion

--- Raices/punto_fijo.py
+++ Raices/punto_fijo.py
@@ -2,16 +2,18 @@ import tabulate as tabulate
 from tabla import tabla
 from tkinter import *
 
+
 def punto_fijo(g, xo, tol, Nmax,  root, operaciones, error, atras):
     if error == "Error absoluto":
         punto_fijo_absoluto(g, xo, tol, Nmax, root, operaciones, atras)
     else:
         punto_fijo_relativo(g, xo, tol, Nmax, root, operaciones, atras)
 
+
 def punto_fijo_absoluto(g, xo, tol, Nmax, root, operaciones, atras):
     resultados = []
     xAnterior = xo
-    gxAnterior = eval (g, operaciones, {'x':xAnterior})
+    gxAnterior = eval(g, operaciones, {'x': xAnterior})
     E = 1000
     resultados.append([0, xo, "N/A"])
     cont = 0
@@ -22,12 +24,12 @@ def punto_fijo_absoluto(g, xo, tol, Nmax, root, operaciones, atras):
         cont += 1
         resultados.append([cont, xActual, E])
         xAnterior = xActual
-        gxAnterior = eval (g, operaciones, {'x':xAnterior})
+        gxAnterior = eval(g, operaciones, {'x': xAnterior})
 
     for widget in root.winfo_children():
         if widget != atras:
             widget.destroy()
-    
+
     e = Entry(root, width=20, fg='Blue', font=('Arial', 10))
     e.grid(row=0, column=0)
     e.insert(END, "Iteración")
@@ -42,10 +44,11 @@ def punto_fijo_absoluto(g, xo, tol, Nmax, root, operaciones, atras):
 
     t = tabla(resultados, root, atras)
 
+
 def punto_fijo_relativo(g, xo, tol, Nmax, root, operaciones, atras):
     resultados = []
     xAnterior = xo
-    gxAnterior = eval (g, operaciones, {'x':xAnterior})
+    gxAnterior = eval(g, operaciones, {'x': xAnterior})
     e = 1000
     resultados.append([0, xo, "N/A"])
     cont = 0
@@ -57,12 +60,12 @@ def punto_fijo_relativo(g, xo, tol, Nmax, root, operaciones, atras):
         cont += 1
         resultados.append([cont, xActual, e])
         xAnterior = xActual
-        gxAnterior = eval (g, operaciones, {'x':xAnterior})
+        gxAnterior = eval(g, operaciones, {'x': xAnterior})
 
     for widget in root.winfo_children():
         if widget != atras:
             widget.destroy()
-    
+
     e = Entry(root, width=20, fg='Blue', font=('Arial', 10))
     e.grid(row=0, column=0)
     e.insert(END, "Iteración")
