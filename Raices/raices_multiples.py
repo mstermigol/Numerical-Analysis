9b5d14142a94d57237ab759d2966116cbcbe997f
Valeria Guerra <vguerraz@eafit.edu.co>
2023-05-31 12:59:05 -0500

    Metodo raices multiples listo

--- Raices/raices_multiples.py
+++ Raices/raices_multiples.py
@@ -7,28 +7,8 @@ def raices_multiples(f, primeraDerivada, segundaDerivada, xo, tol, Nmax,  root, 
         raices_multiples_absoluto(f, primeraDerivada, segundaDerivada, xo, tol, Nmax,  root, operaciones, atras)
     else:
         raices_multiples_relativo(f, primeraDerivada, segundaDerivada, xo, tol, Nmax,  root, operaciones, atras)
-       
-    resultados = []
-    xAnterior = xo
-    fAnterior = f(xAnterior)
-    E = 1000
-    cont = 0
-    resultados.append([cont, xAnterior, fAnterior, "N/A"])
-
-    while E > tol and cont < Nmax:
-        xActual = xAnterior-fAnterior*(primeraDerivada(xAnterior)/(
-            (primeraDerivada(xAnterior))**2-fAnterior*segundaDerivada(xAnterior)))
-        fActual = f(xActual)
-        E = abs(xActual-xAnterior)
-        cont += 1
-        xAnterior = xActual
-        fAnterior = fActual
-        resultados.append([cont, xAnterior, fAnterior, E])
-
-    print(tabulate.tabulate(resultados, headers=[
-        "Iteracion", "Valor de x", "f(x)", "Error"], tablefmt="fancy_grid"))
-
-def raices_multiples_absoluto(f, primeraDerivada, segundaDerivada, xo, tol, Nmax,  root, operaciones, atras):
+ 
+ def raices_multiples_absoluto(f, primeraDerivada, segundaDerivada, xo, tol, Nmax,  root, operaciones, atras):
     resultados = []
     xAnterior = xo
     fAnterior = eval(f, operaciones, {'x': xAnterior})
