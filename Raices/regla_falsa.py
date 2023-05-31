b7c1f1846ca54e68ce9da690b01e0606326290e4
Valeria Guerra <vguerraz@eafit.edu.co>
2023-05-31 12:59:38 -0500

    Metodo regla falsa listo

--- Raices/regla_falsa.py
+++ Raices/regla_falsa.py
@@ -9,30 +9,6 @@ def regla_falsa(f, a, b, tol, Nmax, root, operaciones, error, atras):
     else:
         regla_falsa_relativo(f, a, b, tol, Nmax, root, operaciones, atras)
     
-    #resultados = []
-    #fa = f(a)
-    #fb = f(b)
-    #puntoMedio = (fb*a-fa*b)/(fb-fa)
-    #fPuntoMedio = f(puntoMedio)
-    #E = 1000
-    #cont = 1
-    #resultados.append([0, a, fa, puntoMedio, fPuntoMedio, b, fb, "N/A"])
-
-    #while E > tol and cont < Nmax:
-        #if fa*fPuntoMedio < 0:
-            #b = puntoMedio
-        #else:
-            #a = puntoMedio
-        #p0 = puntoMedio
-        #puntoMedio = (f(b)*a-f(a)*b)/(f(b)-f(a))
-        #fPuntoMedio = f(puntoMedio)
-        #E = abs(puntoMedio-p0)
-        #resultados.append([cont, a, fa, b, fb, puntoMedio, fPuntoMedio, cont])
-        #cont = cont+1
-
-    #print(tabulate.tabulate(resultados, headers=[
-        #"Iteracion", "a", "f(a)", "pm", "f(pm)", "b", "f(b)", "Error"], tablefmt="fancy_grid"))
-
 def regla_falsa_absoluto(f, x0, x1, tol, Nmax, root, operaciones, atras):
     resultados = []
     fa = eval(f, operaciones, {'x': a})
